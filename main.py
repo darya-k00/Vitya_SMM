69df9f9dc32bcce4945903eca96bf017bb0eaae4
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-04-22 23:00:07 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+
+
+def get_post():
+    pass
+
+
+def public_post():
+    pass
+
+
+def main():
+    while True:
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
+    
